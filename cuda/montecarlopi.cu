de2efba803ccbc9a0af36ba14bd21ccae173d5cd
CheHsuan <exas60807@gmail.com>
2016-09-28 00:25:02 +0800

    fix code style

--- cuda/montecarlopi.cu
+++ cuda/montecarlopi.cu
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #define ITERATIONSPERTHREAD 4000
 
-__global__ void monte_carlo_kernel( curandState* state, unsigned int seed, int *numbers) 
+__global__ void monte_carlo_kernel( curandState* state, unsigned int seed, int *numbers)
 {
     unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
     int sum = 0;
@@ -19,7 +19,7 @@ __global__ void monte_carlo_kernel( curandState* state, unsigned int seed, int *
         sum += (x*x + y*y <= 1.0f);
     }
     numbers[index] = sum;
-}   
+}
 
 double compute_pi_montecarlo_gpu(size_t N){
     int threadsPerBlock = 500;
@@ -29,7 +29,7 @@ double compute_pi_montecarlo_gpu(size_t N){
     int *host_nums = (int *)malloc(sizeof(int) * threadsPerBlock * blocksPerGrid);
     if(host_nums == NULL)
         return 0;
-   
+
     // malloc memory in gpu
     cudaMalloc((void **)&devStates, sizeof(curandState) * threadsPerBlock * blocksPerGrid);
     cudaMalloc((void **)&dev_nums, sizeof(int) * threadsPerBlock * blocksPerGrid);
@@ -73,39 +73,39 @@ int main(int argc, char *argv[])
 
 #if defined(MONTECARLO_GPU)
     pi = compute_pi_montecarlo_gpu(N);
-	printf("N = %d , pi = %lf\n", N, pi);
+    printf("N = %d , pi = %lf\n", N, pi);
 #endif
 
 #if defined(MONTECARLO_CPU)
     pi = compute_pi_montecarlo_cpu(N);
-	printf("N = %d , pi = %lf\n", N, pi);
+    printf("N = %d , pi = %lf\n", N, pi);
 #endif
 
 #if defined(BENCHMARK)
 #define CLOCK_ID CLOCK_MONOTONIC_RAW
 #define ONE_SEC 1000000000.0
 
-	struct timespec start = {0, 0};
+    struct timespec start = {0, 0};
     struct timespec end = {0, 0};
 
     if (argc < 2) return -1;
 
     N = atoi(argv[1]);
-    
-	// Monte Carlo with cpu
+
+    // Monte Carlo with cpu
     clock_gettime(CLOCK_ID, &start);
     compute_pi_montecarlo_cpu(N);
     clock_gettime(CLOCK_ID, &end);
     printf("%lf,", (double) (end.tv_sec - start.tv_sec) +
            (end.tv_nsec - start.tv_nsec)/ONE_SEC);
 
-	// Monte Carlo with gpu
+    // Monte Carlo with gpu
     clock_gettime(CLOCK_ID, &start);
     compute_pi_montecarlo_gpu(N);
     clock_gettime(CLOCK_ID, &end);
     printf("%lf\n", (double) (end.tv_sec - start.tv_sec) +
            (end.tv_nsec - start.tv_nsec)/ONE_SEC);
 #endif
- 
-    return 0;  
+
+    return 0;
 }
